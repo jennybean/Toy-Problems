bcf01959d8a242d08a7eaeb339fc2786ed470f19
Jennifer <jmshakala@gmail.com>
2016-09-20 17:57:37 -0500

    first commit

---
+++ hello-world.js
@@ -0,0 +1,5 @@
+// Hello, World! 
+// Write a Hello World! function that can greet someone given their name. The function should return the appropriate greeting.
+// For an input of "Alice", the response should be "Hello, Alice!".
+// If a name is not given, the response should be "Hello, World!"
+
