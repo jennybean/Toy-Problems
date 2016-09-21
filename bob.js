6ebeb1d6e6900de88a8193fbfa68720cfcdd9a13
Jennifer <jmshakala@gmail.com>
2016-09-20 19:01:07 -0500

    commented solution code

--- bob.js
+++ bob.js
@@ -4,3 +4,13 @@
 // He answers 'Whoa, chill out!' if you yell at him.
 // He says 'Fine. Be that way!' if you address him without actually saying anything.
 // He answers 'Whatever.' to anything else.
+
+function bob(convo){
+  // Check if anything is said first, or .includes will break
+	if(!convo) return 'Fine. Be that way!'
+  // Check for questions
+	else if(convo.includes('?')) return 'Sure.'
+  // Check for yelling
+	else if(convo.includes('!')) return 'Whoa, chill out!'
+	else return 'Whatever.'
+}
